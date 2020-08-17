60c82b1bf8c4153a3a65ed71feaa654f47b05963
yonetici <segate@gmail.com>
2020-08-17 17:36:29 +0300

    First day has been completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ProductListComponent} from './products/product-list.component';
+import {ProductListFooterComponent} from './products/product-list-footer.component';
+import {FormsModule} from '@angular/forms';
+import {convertToSpacesPipe} from './shared/convertToSpaces.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductListFooterComponent,
+    convertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/product-list-footer.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'pm-plfooter',
+  templateUrl: './product-list-footer.component.html'
+})
+
+export class ProductListFooterComponent {
+  footerMessage: string = 'Copyright © 2020';
+}
